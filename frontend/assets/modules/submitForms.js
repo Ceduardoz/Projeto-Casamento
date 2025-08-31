ce0caa16481a8f0a2d8b7e456c493aba1bc50055
Ceduardoz <167792627+Ceduardoz@users.noreply.github.com>
2025-08-31 13:12:18 -0300

    vercel

--- frontend/assets/modules/submitForms.js
+++ frontend/assets/modules/submitForms.js
@@ -33,3 +33,22 @@ export function handleFormSubmit() {
     }
   });
 }
+
+// Botão de download
+export function handleDownload() {
+  const btnDownload = document.getElementById("btnDowload");
+
+  btnDownload.addEventListener("click", () => {
+    const senha = prompt("Digite a senha para baixar a lista:");
+
+    // Coloque a senha que você quiser aqui
+    const senhaCorreta = "meusonho123";
+
+    if (senha === senhaCorreta) {
+      window.location.href = "/baixar";
+    } else {
+      alert("Senha incorreta! Você não tem permissão para baixar a lista.");
+    }
+  });
+}
+
